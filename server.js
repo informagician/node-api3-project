4d7b03adf05e6ff269aef3aa97e0ad13ec8e1185
Milo Rastgoo <hi@informagician.com>
2020-02-12 16:47:32 -0500

    validateUserId partially working

--- server.js
+++ server.js
@@ -1,17 +1,47 @@
 const express = require('express');
+const userDb = require('./users/userDb')
 
 const server = express();
+server.use(express.json());
 server.use(logger);
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+server.get('/',(req,res) => {
+  userDb.get().then(users => res.status(200).json(users))
+})
+
+server.get('/:id', validateUserId, (req, res) => {
+  
 });
+
+// server.get('/:id', (req, res) => {
+//   const {id} = req.params;
+//   userDb.getById(id).then(users => {
+//     if(users.length > 0) {
+//       req.user = users
+//     } else {
+//       res.status(400).json({ message: "invalid user id" })
+//     }
+//   })
+// });
 
 //custom middleware
 
 function logger (req,res,next) {
   console.log(`Received a ${req.method} Request from ${req.originalUrl} at ${new Date()}`)
 
+  next();
+}
+
+
+function validateUserId(req,res,next) {
+  const {id} = req.params;
+  userDb.getById(id).then(users => {
+    if(users.length > 0) {
+      req.user = users
+    }
+  }).catch(err => {
+    res.status(400).json({ message: "invalid user id" })
+  })
   next();
 }
 
