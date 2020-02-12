e90b5f7735f83755dfbed479b7c92e2fb57b47f0
Milo Rastgoo <hi@informagician.com>
2020-02-12 15:30:23 -0500

    logger working

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const server = express();
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +9,10 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger (req,res,next) {
+  console.log(`Received a ${req.method} Request from ${req.originalUrl} at ${new Date()}`)
+
+  next();
+}
 
 module.exports = server;
